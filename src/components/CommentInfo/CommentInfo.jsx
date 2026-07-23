f45822a13ddc7023ed5dea939056d12c20dcaba5
Anton Ustavshchykov <unton84@gmail.com>
2026-07-23 17:34:40 +0300

    Solution

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,1 +1,15 @@
-export const CommentInfo = () => <>Put the comment here</>;
+export const CommentInfo = ({ comments }) => (
+  <div className="CommentInfo">
+    <div className="CommentInfo__title">
+      <strong className="CommentInfo__name">{comments.name}</strong>
+
+      {' by '}
+
+      <a className="CommentInfo__email" href={`mailto:${comments.email}`}>
+        {comments.email}
+      </a>
+    </div>
+
+    <div className="CommentInfo__body"> {comments.body} </div>
+  </div>
+);
--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,8 @@
-export const CommentList = () => <>Put the list here</>;
+import './CommentList.scss';
+import { CommentInfo } from '../CommentInfo/CommentInfo';
+
+export const CommentList = ({ comments }) => (
+  <div className="CommentList">
+    <CommentInfo comments={comments} />
+  </div>
+);
