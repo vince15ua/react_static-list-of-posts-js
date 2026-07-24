7ccaace6fd8ac4bbff59d1cf45e3c26077e2f605
Anton Ustavshchykov <unton84@gmail.com>
2026-07-24 16:21:27 +0300

    iteration fix

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -3,6 +3,8 @@ import { CommentInfo } from '../CommentInfo/CommentInfo';
 
 export const CommentList = ({ comments }) => (
   <div className="CommentList">
-    <CommentInfo comment={comments} />
+    {comments.map(comment => (
+      <CommentInfo key={comment.id} comment={comment} />
+    ))}
   </div>
 );
